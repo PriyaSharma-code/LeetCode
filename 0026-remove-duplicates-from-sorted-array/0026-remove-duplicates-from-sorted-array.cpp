9fe73b7a2882d2b26c73af3ce69b71a6c972d414
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-04-24 09:33:27 +0530

    Time: 0 ms (100%), Space: 22.6 MB (50.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 1;              // Start from the second element
-        int nonDupe = 1;        // Position to insert the next unique element
+        if (nums.empty()) return 0;
 
-        while (i < nums.size()) {
-            // If the current element is not equal to the previous one,
-            // it means it's a new unique element
-            if (nums[i - 1] != nums[i]) {
-                nums[nonDupe] = nums[i]; // Place the unique element at the 'nonDupe' index
-                nonDupe++;               // Move the pointer for the next unique element
+        int count = 1; 
+
+        for(int i = 1; i < nums.size(); ++i){
+            if (nums[i] != nums[count - 1]){
+                nums[count] = nums[i];
+                count++;
             }
-            i++; // Move to the next element
         }
 
-        // 'nonDupe' represents the number of unique elements in the array
-        return nonDupe;
+        return count;
     }
 };
