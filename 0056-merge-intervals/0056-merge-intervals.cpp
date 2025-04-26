bb37af5bd670e8b045939ac1cc6d845c6e4cfdec
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-04-26 20:28:16 +0530

    Time: 7 ms (43.7%), Space: 24 MB (35.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,17 +6,20 @@ public:
         }
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        vector<int> cur = intervals[0];
+        //vector<int> cur = intervals[0];
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]<= cur[1]){
-                cur[1] = max(cur[1],intervals[i][1]);
+            if(intervals[i][0]<= end){
+               end = max(end,intervals[i][1]);
             }
             else{
-                ans.push_back(cur);
-                cur = intervals[i];
+                ans.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
-        ans.push_back(cur);
+        ans.push_back({start,end});
         return ans;
     }
 };
