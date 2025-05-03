9e01499489ce74a6c35ae43d2eccdabb83daec00
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-05-04 00:11:30 +0530

    Time: 8 ms (29.53%), Space: 115.5 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int res = INT_MAX;
 
-        int face[7] = {};
-        for(int i = 0; i < tops.size(); i++) {
-            face[tops[i]]++;
-            if(bottoms[i] != tops[i]) face[bottoms[i]]++;
+        int n = tops.size();
+        vector<int> top(7, 0), bottom(7, 0), same(7, 0);
+
+        for(int i=0;i<tops.size();i++) {
+            top[tops[i]]++;
+            bottom[bottoms[i]]++;
+            if(tops[i] == bottoms[i]) 
+                same[tops[i]]++;
         }
 
-        // for each possible face from 1...6
-        for(int x = 1; x <= 6; x++) {
-            if(face[x] < tops.size()) continue;      // can't fill entire row
-
-            int maintainTop = 0, maintainBottom = 0;
-            bool possible = true;
-
-            // count flips
-            for(int i = 0; i < tops.size(); i++) {
-                if (tops[i] != x)      maintainTop++;
-                if (bottoms[i] != x)   maintainBottom++;
+        int ans = INT_MAX;
+        for(int i=0;i<7;i++) {
+            if(top[i] + bottom[i] - same[i] == n) {
+                ans = min({ans, n - top[i], n - bottom[i]});
             }
-
-            if (possible) 
-                res = min(res, min(maintainTop, maintainBottom));
         }
 
-        return res == INT_MAX ? -1 : res;
+        return ans == INT_MAX ? -1 : ans;
+        
     }
 };
