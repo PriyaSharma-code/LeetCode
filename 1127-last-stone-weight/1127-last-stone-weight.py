66908f67a9999165159598654c93565f638c7b4b
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-05-22 10:29:17 +0530

    Time: 0 ms (100%), Space: 17.7 MB (50.72%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -1,23 +1,9 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones.sort(reverse=True)
+        h = [-x for x in stones]
+        heapq.heapify(h)
 
-        while len(stones) > 1:
-            stone1 = stones.pop(0)
-            stone2 = stones.pop(0)
-
-            new_weight = abs(stone1 - stone2)
-
-            if new_weight == 0: continue
-
-            insert_index = len(stones)
-            for i in range(len(stones)):
-                if stones[i] < new_weight:
-                    insert_index = i
-                    break
-
-            stones.insert(insert_index, new_weight)
-
-        if len(stones) == 0: return 0
-
-        return stones[0]
+        while len(h)>1 and h[0]!=0:
+            heapq.heappush(h,heapq.heappop(h) - heapq.heappop(h))
+        
+        return -h[0]
