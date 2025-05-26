52177eb935162e74705949441b117ace2e4963db
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-05-26 22:47:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (8.49%) - LeetHub

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.cpp
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int minDeletion(string s, int k) {
-        int n = s.length();
-        if(k>n){
-            return 0;
-        }
         unordered_map<char,int> freq;
-        for(int i = 0; i <n ;i++){
+        for(int i=0;i<s.size();i++){
             freq[s[i]]++;
         }
-        int x = freq.size();
-        if(x<=k) return 0;
-        int z = x - k;
+        vector<pair<char, int>> freqVec(freq.begin(), freq.end());
+    sort(freqVec.begin(), freqVec.end(), [](pair<char, int>& a, pair<char, int>& b) {
+        return a.second < b.second; 
+    });
+    int val = freq.size() - k;
+    int ans = 0;
+    for(int i=0;i<val;i++){
+        ans += freqVec[i].second;
+    }
 
-        vector<pair<char,int>> vec(freq.begin(),freq.end());
-        sort(vec.begin(),vec.end(),[](pair<char,int> &a, pair<char,int> &b){
-            return a.second<b.second;
-        });
+    return ans;
 
-        int sum = 0;
-        for(int i = 0 ; i<z;i++){
-            sum+= vec[i].second;
-        }
-        return sum;
-
+        
     }
 };
