31efd6d3f45be188f735049608a5c0413bf89992
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-07-26 00:47:40 +0530

    Time: 3 ms (25%), Space: 30.1 MB (5.24%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        unordered_set<int> positiveNumsSet;
-        for (int num : nums) {
-            if (num > 0) {
-                positiveNumsSet.emplace(num);
-            }
-        }
-        if (positiveNumsSet.empty()) {
-            return *max_element(nums.begin(), nums.end());
-        }
-        return accumulate(positiveNumsSet.begin(), positiveNumsSet.end(), 0);
+        unordered_set<int> s(nums.begin(),nums.end()); //set
+        return accumulate(s.begin(),s.end(),INT_MIN/2,[](int a,int b){
+            return max({a,a+b,b});
+        });
     }
 };
