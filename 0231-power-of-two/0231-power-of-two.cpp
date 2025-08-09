ecaad9122dea492f282c20e766b1267462cf2265
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-08-09 22:42:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <=0){
+        if (n > 0 && (n & (n - 1))) {
             return false;
+        } else {
+            return n > 0;
         }
-        return ceil(log2(n)) == floor(log2(n));
     }
 };
