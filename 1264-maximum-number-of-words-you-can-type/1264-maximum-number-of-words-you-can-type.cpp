d9785bfbb5994b13d8a132890a943afdd712104d
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2025-09-16 09:27:32 +0530

    Time: 2 ms (28.57%), Space: 10.2 MB (5.32%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char> broken;
+        int count=0;
+
+        for(auto &c: brokenLetters){
+            broken.insert(c);
+        }
+        stringstream ss(text);
+        string word;
+        vector<string> words;
+
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        for(auto w : words){
+            for(char c: w){
+                if(broken.find(c)!=broken.end()){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return words.size() - count;
+
+    }
+};
