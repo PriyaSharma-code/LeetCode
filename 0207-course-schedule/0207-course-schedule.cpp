cf9f75063f718b11216ccc9459c2b18afd834829
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-03-17 09:22:51 +0530

    Time: 2 ms (80.57%), Space: 19.1 MB (81.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses,0), bfs;
+        vector<int> indegree(numCourses,0);
 
         for(auto &e: prerequisites){
             graph[e[1]].push_back(e[0]);
             indegree[e[0]]++;
     }
-
-        for(int i=0 ;i<numCourses; i++){
-            if(indegree[i]==0){
-                bfs.push_back(i);
+    queue<int> q;
+    for(int i = 0; i<numCourses; i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    int count = 0;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        count++;
+        for(int next : graph[node]){
+            indegree[next]--;
+            if(indegree[next]==0){
+                q.push(next);
             }
         }
-
-        for(int i=0; i< bfs.size(); i++){
-            for(int j : graph[bfs[i]]){
-                if(--indegree[j]==0){
-                    bfs.push_back(j);
-                }
-            }
-        }
-        return bfs.size()==numCourses;
+    }
+       
+        return count ==numCourses;
     }
 };
