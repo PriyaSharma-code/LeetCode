e0dfb6fa345703bfb2170ac3d9a6b8b1d5bce95e
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-03-20 15:52:20 +0530

    Time: 7 ms (41.98%), Space: 26.4 MB (27.55%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        set<int> ans;
         int majority = int(nums.size()/3);
-        map<int,int> mp;
+        vector<int> ans;
+        unordered_map<int,int> mp;
 
         for(int i = 0 ; i<nums.size();i++){
             mp[nums[i]]++;
-            if(mp[nums[i]]>majority){
-                ans.insert(nums[i]);
+        }
+        for(auto &e: mp){
+            if(e.second>majority){
+                ans.push_back(e.first);
             }
         }
-        vector<int> a;
-        for(auto&e: ans){
-            a.push_back(e);
-        }
-        return a;
+        return ans;
     }
 };
