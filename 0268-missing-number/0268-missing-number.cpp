a77d6fe19cff7fc27bcaa25fccf4a8a781fec747
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-03-22 11:41:05 +0530

    Time: 0 ms (100%), Space: 21.7 MB (69.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int expectedSum = 0;
-        int curSum=0;
-        int n = nums.size();
 
-        expectedSum = n*(n+1)/2;
 
-        for(int num : nums){
-            curSum+=num;
+int cycsort(vector<int>&arr){
+    int i=0;
+    while(i<arr.size()){
+        int correctind=arr[i];
+       
+        if(i != correctind && arr[i] <arr.size())
+        swap(arr[i],arr[correctind]);
+
+        else 
+        i++;
+    }
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i] != i){
+            return i;
         }
+    }
+    return arr.size();
+}
+    int missingNumber(vector<int>& nums) {
 
-        return expectedSum - curSum;
+        return cycsort(nums);
     }
 };
