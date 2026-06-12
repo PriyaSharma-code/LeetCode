a135a70d61867af6a2e64bbf814781b34a36a306
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-06-12 11:19:26 +0530

    Time: 3 ms (3.07%), Space: 16.6 MB (38.07%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,53 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (!head || left == right) return head;
-        
+    ListNode* reverseBetween(ListNode* head, int left, int right){
+        if(!head || left==right){
+            return head;
+        }
         ListNode dummy(0);
         dummy.next = head;
         ListNode* prev = &dummy;
-        if(left!=0){
-        for (int i = 0; i < left - 1; ++i) {
+
+        for(int i=0; i<left-1;i++){
             prev = prev->next;
         }
+        ListNode* cur = prev->next;
+        for(int i=0; i<right - left;i++){
+            ListNode* next_n = cur->next;
+            cur->next = next_n->next;
+            next_n->next = prev->next;
+            prev->next = next_n;
         }
-        ListNode* current = prev->next;
-        
-        
-        for (int i = 0; i < right - left; ++i) {
-            ListNode* next_node = current->next;
-            current->next = next_node->next;
-            next_node->next = prev->next;
-            prev->next = next_node;
-        }
-        
         return dummy.next;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head) {
-            return NULL;
-        }
-        if (!head || k == 1) return head;
+        if(!head){ return NULL;}
+        if(k == 1){return head;}
 
         ListNode dummy(0);
         dummy.next = head;
         ListNode* prev = &dummy;
-        ListNode* curr = head;
+        ListNode* curr =head;
         ListNode* next = nullptr;
+
         int count = 0;
-
-        // Count the total number of nodes
-        while (curr) {
+        while(curr){
             count++;
-            curr = curr->next;
+            curr=curr->next;
         }
-
         curr = head;
-
-        // Reverse in groups of `k`
-        while (count >= k) {
-            for (int i = 1; i < k; ++i) {
+        while(count>=k){
+            for(int i=1;i<k;i++){
                 next = curr->next;
                 curr->next = next->next;
                 next->next = prev->next;
@@ -64,9 +55,8 @@ public:
             }
             prev = curr;
             curr = curr->next;
-            count -= k;
+            count -=k;
         }
-
         return dummy.next;
     }
 };
