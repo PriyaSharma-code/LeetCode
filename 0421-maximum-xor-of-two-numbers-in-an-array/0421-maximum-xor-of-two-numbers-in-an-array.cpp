b3e42be5d6fd1ab7f5c8b0b86099fbe1f27c357e
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-06-13 21:48:15 +0530

    Time: 110 ms (99.9%), Space: 61.1 MB (100%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,51 +1,31 @@
-class Node {
-    Node* links[2];
-
-public:
-    bool contains_key(int bit) { return (links[bit] != NULL); }
-    void put(int bit, Node* temp) { links[bit] = temp; }
-    Node* get(int bit) { return links[bit]; }
-};
-class Trie {
-    Node* root;
-
-public:
-    Trie() { root = new Node(); }
-    void insert(int num) {
-        Node* node = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = ((num >> i) & 1);
-            if (!node->contains_key(bit)) {
-                node->put(bit, new Node());
-            }
-            node = node->get(bit);
-        }
-    }
-    int max_xor(int num) {
-        int mx = 0;
-        Node* node = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = ((num >> i) & 1);
-            if (node->contains_key(!bit)) {
-                mx = ((1 << i) | mx);
-                node = node->get(!bit);
-            } else
-                node = node->get(bit);
-        }
-        return mx;
-    }
-};
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int mx = 0;
-        Trie trie;
-        for (auto i : nums)
-            trie.insert(i);
-        for (auto i : nums)
-            mx = max(mx, trie.max_xor(i));
-        return mx;
+        sort( nums.begin(), nums.end( ) );
+        nums.erase(unique( nums.begin( ), nums.end( ) ), nums.end( ) );
+        const int size = static_cast<int>( nums.size( ) );
+        int l = 0;
+        int r = size - 1;
+        int result = nums.back( ) ^ nums.front( );
+        long long upper = 1;
+        while( upper <= nums.back( ) )
+            upper <<= 1;
+        --upper;
+        while( l + 1 < r )
+        {
+            int a = nums[l];
+            int b = nums[l + 1];
+            int c = nums[r - 1];
+            int d = nums[r];
+            result = max( result, a ^ b );
+            result = max( result, a ^ c );
+            result = max( result, b ^ d );
+            result = max( result, c ^ d );
+            if( static_cast<long long>( a ) + d < upper )
+                ++l;
+            else
+                --r;
+        }
+        return result;
     }
 };
-
-// Source- Striver Trie playlist
