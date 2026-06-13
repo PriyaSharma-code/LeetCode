0261380fea267357246d23c7043966eaac77f86d
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-06-13 21:46:16 +0530

    Time: 572 ms (86.46%), Space: 184.1 MB (77.53%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,46 +1,51 @@
-class TrieNode{
+class Node {
+    Node* links[2];
+
 public:
-    TrieNode *child[2];
-    
-    TrieNode(){
-        this->child[0] = NULL; //for 0 bit 
-        this->child[1] = NULL; //for 1 bit
+    bool contains_key(int bit) { return (links[bit] != NULL); }
+    void put(int bit, Node* temp) { links[bit] = temp; }
+    Node* get(int bit) { return links[bit]; }
+};
+class Trie {
+    Node* root;
+
+public:
+    Trie() { root = new Node(); }
+    void insert(int num) {
+        Node* node = root;
+        for (int i = 31; i >= 0; i--) {
+            int bit = ((num >> i) & 1);
+            if (!node->contains_key(bit)) {
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+    int max_xor(int num) {
+        int mx = 0;
+        Node* node = root;
+        for (int i = 31; i >= 0; i--) {
+            int bit = ((num >> i) & 1);
+            if (node->contains_key(!bit)) {
+                mx = ((1 << i) | mx);
+                node = node->get(!bit);
+            } else
+                node = node->get(bit);
+        }
+        return mx;
     }
 };
 class Solution {
-    TrieNode *newNode;
-    
-    void insert(int x){   //to insert each element into the Trie
-        TrieNode *t = newNode;
-        bitset<32> bs(x);
-        
-        for(int j=31; j>=0; j--){
-            if(!t->child[bs[j]]) t->child[bs[j]] = new TrieNode(); //start from the MSB =, move to LSB using bitset
-            t = t->child[bs[j]];
-        }    
-    }
-    
 public:
     int findMaximumXOR(vector<int>& nums) {
-        newNode = new TrieNode();
-        for(auto &n : nums) insert(n); //insert all the elements into the Trie
-        
-        int ans=0; //Stores the maximum XOR possible so far
-        for(auto n : nums){
-            ans = max(ans, maxXOR(n));  //updates the ans as we traverse the array & compute max XORs at each element.
-        }
-        return ans;
-    }
-    
-    int maxXOR(int n){
-        TrieNode *t = newNode;
-        bitset<32> bs(n);
-        int ans=0; 
-        for(int j=31; j>=0; j--){
-            if(t->child[!bs[j]]) ans += (1<<j), t = t->child[!bs[j]]; //Since 1^0 = 1 & 1^1 = 0, 0^0 = 0
-           
-            else t = t->child[bs[j]];
-        }
-        return ans;
+        int mx = 0;
+        Trie trie;
+        for (auto i : nums)
+            trie.insert(i);
+        for (auto i : nums)
+            mx = max(mx, trie.max_xor(i));
+        return mx;
     }
 };
+
+// Source- Striver Trie playlist
