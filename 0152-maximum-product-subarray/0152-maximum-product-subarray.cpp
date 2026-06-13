3a5fa4848442972236d3e452d85db31205652eac
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-06-13 21:04:59 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        int maxprod=nums[0];
+        int minprod=nums[0];
+        int ans =nums[0];
 
-        // Stores the maximum product found so far.
-        // Initialized to the smallest possible integer.
-        int maxi = INT_MIN;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] <0) swap(maxprod,minprod);
 
-        // Product while traversing from left to right.
-        int left = 1;
+            maxprod=max(nums[i],nums[i]*maxprod);
+            minprod=min(nums[i],nums[i]*minprod);
 
-        // Product while traversing from right to left.
-        int right = 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            // Multiply current number into the left-running product.
-            left *= nums[i];
-
-            // Multiply current number into the right-running product.
-            // nums.size() - i - 1 gives indices:
-            // n-1, n-2, n-3, ...
-            right *= nums[nums.size() - i - 1];
-
-            // Update maximum product using both directions.
-            // We check both because negative numbers can flip signs.
-            maxi = max(maxi, max(left, right));
-
-            // If left product becomes 0,
-            // start a new subarray after this zero.
-            if (left == 0) {
-                left = 1;   // reset
-            }
-
-            // If right product becomes 0,
-            // start a new subarray before this zero.
-            if (right == 0) {
-                right = 1;  // reset
-            }
+            ans=max(ans,maxprod);
         }
-
-        // Return the maximum product subarray found.
-        return maxi;
+        return ans;
     }
 };
