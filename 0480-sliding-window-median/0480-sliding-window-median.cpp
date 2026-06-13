323f833afd5b0ef8a18ed5e8ddc63cf210cb32bd
Priya_Sharma <65938994+PriyaSharma-code@users.noreply.github.com>
2026-06-13 10:45:28 +0530

    Time: 36 ms (91.59%), Space: 41.7 MB (67.92%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,104 +1,132 @@
 class Solution {
 public:
-    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<double> ans;
-        for (int i = 0; i < k-1; i++)
-            addNumber(i, 0, nums);
-        for (int i = k-1; i < n; i++) {
-            addNumber(i, i-k+1, nums);
-            ans.push_back(calc(k, i-k+1));
-            removeNumber(i-k+1, i-k+1, nums);
+
+    priority_queue<long long> small;
+
+    priority_queue<long long,
+                   vector<long long>,
+                   greater<long long>> large;
+
+    unordered_map<long long,int> delayed;
+
+    int smallSize = 0;
+    int largeSize = 0;
+
+    void pruneSmall() {
+        while (!small.empty()) {
+            long long x = small.top();
+
+            if (delayed.count(x) && delayed[x] > 0) {
+                delayed[x]--;
+                small.pop();
+            }
+            else
+                break;
         }
+    }
+
+    void pruneLarge() {
+        while (!large.empty()) {
+            long long x = large.top();
+
+            if (delayed.count(x) && delayed[x] > 0) {
+                delayed[x]--;
+                large.pop();
+            }
+            else
+                break;
+        }
+    }
+
+    void balance() {
+
+        if (smallSize > largeSize + 1) {
+
+            large.push(small.top());
+            small.pop();
+
+            smallSize--;
+            largeSize++;
+
+            pruneSmall();
+        }
+
+        else if (smallSize < largeSize) {
+
+            small.push(large.top());
+            large.pop();
+
+            smallSize++;
+            largeSize--;
+
+            pruneLarge();
+        }
+    }
+
+    void addNum(long long num) {
+
+        if (small.empty() || num <= small.top()) {
+            small.push(num);
+            smallSize++;
+        }
+        else {
+            large.push(num);
+            largeSize++;
+        }
+
+        balance();
+    }
+
+    void removeNum(long long num) {
+
+        delayed[num]++;
+
+        if (num <= small.top()) {
+
+            smallSize--;
+
+            if (num == small.top())
+                pruneSmall();
+        }
+        else {
+
+            largeSize--;
+
+            if (num == large.top())
+                pruneLarge();
+        }
+
+        balance();
+    }
+
+    double getMedian(int k) {
+
+        if (k % 2)
+            return (double)small.top();
+
+        return ((double)small.top() +
+                (double)large.top()) / 2.0;
+    }
+
+    vector<double> medianSlidingWindow(vector<int>& nums,
+                                       int k) {
+
+        vector<double> ans;
+
+        for (int i = 0; i < k; i++)
+            addNum(nums[i]);
+
+        ans.push_back(getMedian(k));
+
+        for (int i = k; i < nums.size(); i++) {
+
+            addNum(nums[i]);
+
+            removeNum(nums[i - k]);
+
+            ans.push_back(getMedian(k));
+        }
+
         return ans;
     }
-
-private:
-    priority_queue<pair<int, int>> maxHeap;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-    int minHeapSize = 0;
-    int maxHeapSize = 0;
-
-    void cleanMaxHeap(int l) {
-        while (!maxHeap.empty()) {
-            if (maxHeap.top().second >= l)
-                break;
-            maxHeap.pop();
-        }
-        return;
-    }
-
-    void cleanMinHeap(int l) {
-        while (!minHeap.empty()) {
-            if (minHeap.top().second >= l)
-                break;
-            minHeap.pop();
-        }
-        return;
-    }
-
-    void adjustHeaps(int l) {
-        cleanMaxHeap(l);
-        cleanMinHeap(l);
-        if (maxHeapSize == minHeapSize+1) {
-            if (!maxHeap.empty()) {
-                pair<int, int> t = maxHeap.top();
-                maxHeap.pop();
-                maxHeapSize--;
-                minHeap.push(t);
-                minHeapSize++;
-            }
-        }
-        if (maxHeapSize == minHeapSize-2) {
-            if (!minHeap.empty()) {
-                pair<int, int> t = minHeap.top();
-                minHeap.pop();
-                minHeapSize--;
-                maxHeap.push(t);
-                maxHeapSize++;
-            }
-        }
-
-        if (maxHeapSize > 0) {
-            pair<int, int> maxHeapTop = maxHeap.top();
-            pair<int, int> minHeapTop = minHeap.top();
-            if (maxHeapTop > minHeapTop) {
-                maxHeap.pop();
-                minHeap.pop();
-                maxHeap.push(minHeapTop);
-                minHeap.push(maxHeapTop);
-            }
-        }
-    }
-
-    void addNumber(int ind, int l, vector<int> &nums) {
-        minHeap.push({nums[ind], ind});
-        minHeapSize++;
-        adjustHeaps(l);
-        // test("add");
-    }
-
-    void removeNumber(int ind, int l, vector<int> &nums) {
-        cleanMinHeap(l);
-        pair<int, int> t = minHeap.top();
-        pair<int, int> cur = {nums[ind], ind};
-
-        if (cur >= t) {
-            minHeapSize--;
-        } else {
-            maxHeapSize--;
-        }
-        adjustHeaps(l+1);
-        // test("remove");
-    }
-
-    double calc(int k, int l) {
-        cleanMaxHeap(l);
-        cleanMinHeap(l);
-        // test("calc");
-        if (k&1)
-            return (double) (minHeap.top().first);
-        return (double) ((long long) minHeap.top().first + (long long) maxHeap.top().first)/2.0;
-    }
-
 };
